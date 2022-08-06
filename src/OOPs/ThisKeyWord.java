5e2e0e122673adea18671ce2bc6ab51d65b5e2d3
Saiprasad Subhash Patil <110680594+Saiprasad1010@users.noreply.github.com>
2022-08-06 17:31:25 +0530

    Update ThisKeyWord.java

--- src/OOPs/ThisKeyWord.java
+++ src/OOPs/ThisKeyWord.java
@@ -13,21 +13,21 @@ public class ThisKeyWord {
 		this.abc();
 		System.out.println(this.a);
 	}
-//	void abc2() {
-//		System.out.println("No Argument Cons");
-//		this.abc1();
-//	}
-//	void abc3() {
-//		this.abc2();
-//		int a=45,b=5;
-//		System.out.println(a+b);
-//		System.out.println(this.a);
-//	}
-//	ThisKeyWord(){
-//	this.abc1();
-//	int a=12;
-//	System.out.println("Saiprasad");
-//	}
+	void abc2() {
+		System.out.println("No Argument Cons");
+		this.abc1();
+	}
+	void abc3() {
+		this.abc2();
+		int a=45,b=5;
+		System.out.println(a+b);
+		System.out.println(this.a);
+	}
+	ThisKeyWord(){
+	this.abc1();
+	int a=12;
+	System.out.println("Saiprasad");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ThisKeyWord f=new ThisKeyWord();
